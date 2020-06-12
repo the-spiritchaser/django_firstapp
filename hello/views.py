b4087b49fb61cd1bcf00e2a0c7fff83efbb078e1
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-06-12 17:38:58 +0900

    plaything change index

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request, id, nickname):
-    result = 'your id: ' + str(id) + ', name: "' \
-        + nickname + '".'
+def index(request, nickname, age):
+    result = 'your account: ' + nickname + '" (' + str(age) + ').'
     return HttpResponse(result)
