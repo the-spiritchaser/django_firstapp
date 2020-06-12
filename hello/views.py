347d33b7407cef1b7fec92f840f1a356045edaa1
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-06-12 17:15:38 +0900

    [add] git ignore

--- hello/views.py
+++ hello/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    msg = request.GET['msg']
-    return HttpResponse('you typed: "' + msg + '".')
+    if 'msg' in request.GET:
+        msg = request.GET['msg']
+        return HttpResponse('you typed: "' + msg + '".')
+    else:
+        result = 'please send msg paramerer.'
+    return HttpResponse(result)
