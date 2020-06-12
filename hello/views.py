1ad4a2cdda24156cbea22ac5f9a24c72f3514213
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-06-12 16:57:43 +0900

    use query params

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World")
+    msg = request.GET['msg']
+    return HttpResponse('you typed: "' + msg + '".')
