0feff85bbb8fbf143cd11a6c8955c245eb419654
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-06-12 17:28:11 +0900

    update index params

--- hello/urls.py
+++ hello/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('<int:id>/<nickname>/', views.index, name='index'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    if 'msg' in request.GET:
-        msg = request.GET['msg']
-        return HttpResponse('you typed: "' + msg + '".')
-    else:
-        result = 'please send msg paramerer.'
+def index(request, id, nickname):
+    result = 'your id: ' + str(id) + ', name: "' \
+        + nickname + '".'
     return HttpResponse(result)
