60fe558dcbf98cd362d983b4dafc557bc94220a9
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-06-13 12:43:00 +0900

    use variable

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'hello/index.html')
+    params = {
+        'title':'Hello/Index',
+        'msg':'サンプルページです',
+    }
+    return render(request, 'hello/index.html', params)
