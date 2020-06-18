63407a0e1dc0f6eee9fec8e1220009fab1f5d538
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-06-18 20:47:05 +0900

    update urls

--- hello/urls.py
+++ hello/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from . import views
+from django.conf.urls import url
+from .views import HelloView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    url(r'', HelloView.as_view(), name='index'),
 ]
