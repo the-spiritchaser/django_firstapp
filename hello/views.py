39a2272e547c42939774c9a10da0c2a9dce3a54e
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-06-24 18:22:08 +0900

    use database

--- hello/views.py
+++ hello/views.py
@@ -1,24 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
-from .forms import HelloForm
+from .models import Friend
 
-class HelloView(TemplateView):
-    
-    def __init__(self):
-        self.params = {
-                'title': 'Hello',
-                'form': HelloForm(),
-                'result':None
-            }
-    
-    def get(self, request):
-        return render(request, 'hello/index.html', self.params)
-
-    def post(self, request):
-        if ('check' in request.POST):
-            self.params['result'] = 'Checked.'
-        else:
-            self.params['result'] = 'Not Checked.'
-        self.params['form'] = HelloForm(request.POST)
-        return render(request, 'hello/index.html', self.params)
+def index(request):
+    data = Friend.objects.all()
+    params = {
+            'title': 'Hello',
+            'message': 'all friends.',
+            'data': data,
+        }
+    return render(request, 'hello/index.html', params)
