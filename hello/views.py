15d1d2dab431bc03997e790e44c0a23411c44e6b
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-06-28 13:30:45 +0900

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from .models import Friend
-from .forms import HelloForm
+from .forms import FriendForm
 
 def index(request):
     data = Friend.objects.all()
@@ -12,20 +12,14 @@ def index(request):
         }
     return render(request, 'hello/index.html', params)
 
-# create model
 def create(request):
-    params = {
-        'title': 'Hello',
-        'form': HelloForm(),
-    }
     if (request.method == 'POST'):
-        name = request.POST['name']
-        mail = request.POST['mail']
-        gender = 'gender' in request.POST
-        age = int(request.POST['age'])
-        birth = request.POST['birthday']
-        friend = Friend(name=name,mail=mail,gender=gender,\
-                age=age,birthday=birth)
+        obj = Friend()
+        friend = FriendForm(request.POST, instance=obj)
         friend.save()
         return redirect(to='/hello')
+    params = {
+        'title': 'Hello',
+        'form': FriendForm(),
+    }
     return render(request, 'hello/create.html', params)
