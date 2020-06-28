ca5a9abccca4dd7653dbcc41aec4a884b3b9e43c
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-06-28 13:11:40 +0900

    add create method

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from .models import Friend
+from .forms import HelloForm
 
 def index(request):
     data = Friend.objects.all()
     params = {
             'title': 'Hello',
-            'message': 'all friends.',
             'data': data,
         }
     return render(request, 'hello/index.html', params)
+
+# create model
+def create(request):
+    params = {
+        'title': 'Hello',
+        'form': HelloForm(),
+    }
+    if (request.method == 'POST'):
+        name = request.POST['name']
+        mail = request.POST['mail']
+        gender = 'gender' in request.POST
+        age = int(request.POST['age'])
+        birth = request.POST['birthday']
+        friend = Friend(name=name,mail=mail,gender=gender,\
+                age=age,birthday=birth)
+        friend.save()
+        return redirect(to='/hello')
+    return render(request, 'hello/create.html', params)
