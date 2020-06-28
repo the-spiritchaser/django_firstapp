ac04e6818893d261c321cf1590dbcd2405f65710
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-06-28 13:30:41 +0900

    Update forms.py

--- hello/forms.py
+++ hello/forms.py
@@ -1,4 +1,10 @@
 from django import forms
+from.models import Friend
+
+class FriendForm(forms.ModelForm):
+    class Meta:
+        model = Friend
+        fields = ['name','mail','gender','age','birthday']
 
 class HelloForm(forms.Form):
     name = forms.CharField(label='Name')
