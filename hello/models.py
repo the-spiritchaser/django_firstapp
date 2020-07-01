01fe56dffab1005e9260e7154277db8864b0176f
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-07-01 14:10:23 +0900

    use regexValidator

--- hello/models.py
+++ hello/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.core.validators import MinLengthValidator
+from django.core.validators import RegexValidator
 
 class Friend(models.Model):
     name = models.CharField(max_length=100, \
-        validators=[MinLengthValidator(10)])
-    mail = models.EmailField(max_length=200, \
-         validators=[MinLengthValidator(10)])
+        validators=[RegexValidator(r'^[a-z]*$')])
+    mail = models.EmailField(max_length=200)
     gender = models.BooleanField()
     age = models.IntegerField()
     birthday = models.DateField()
