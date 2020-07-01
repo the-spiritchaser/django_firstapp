62c9571268281abee50ce3e3c9394caf7c07149e
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-07-01 13:54:37 +0900

    add validator

--- hello/models.py
+++ hello/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Friend(models.Model):
     name = models.CharField(max_length=100)
     mail = models.EmailField(max_length=200)
     gender = models.BooleanField()
-    age = models.IntegerField(default=0)
+    age = models.IntegerField(validators=[ \
+          MinValueValidator(0), \
+          MaxValueValidator(150)])
     birthday = models.DateField()
      
     def __str__(self):
