6a4dbbf27d2271ebfec560287c984f53798015df
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-07-06 18:36:45 +0900

    add post form

--- sns/forms.py
+++ sns/forms.py
@@ -55,3 +55,15 @@ class FriendsForm(forms.Form):
 class CreateGroupForm(forms.Form):
     group_name = forms.CharField(max_length=50)
 
+class PostForm(forms.Form):
+    content = forms.CharField(max_length=500, \
+            widget=forms.Textarea)
+    
+    def __init__(self, user, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        public = User.objects.filter(username='public').first()
+        self.fields['groups'] = forms.ChoiceField(
+            choices=[('-','-')] + [(item.title, item.title) \
+                     for item in Group.objects. \
+                     filter(owner__in=[user,public])],
+        )
