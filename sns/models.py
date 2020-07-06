3827a5387cbc8335c8a9cb2161f7560fb474e83f
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-07-06 17:15:24 +0900

    add Friend model

--- sns/models.py
+++ sns/models.py
@@ -28,4 +28,13 @@ class Group(models.Model):
     def __str__(self):
         return self.title
 
+class Friend(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, \
+            related_name='friend_owner')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+        
+    def __str__(self):
+        return str(self.user) + ' (group:"' + str(self.group) + '")'
+
 
