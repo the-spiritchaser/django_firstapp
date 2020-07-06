e1a286d6413072d75aa9164199646d4b0a2df78c
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-07-06 17:14:04 +0900

    add Message model

--- sns/models.py
+++ sns/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Message(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, \
+            related_name='message_owner')
+    group = models.ForeignKey('Group', on_delete=models.CASCADE)
+    content = models.TextField(max_length=1000)
+    share_id = models.IntegerField(default=-1)
+    good_count = models.IntegerField(default=0)
+    share_count = models.IntegerField(default=0)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.content) + ' (' + str(self.owner) + ')'
+    
+    def get_share(self):
+        return Message.objects.get(id=self.share_id)
+
+    class Meta:
+        ordering = ('-pub_date',)
