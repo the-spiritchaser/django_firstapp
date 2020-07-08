ec24e31856fbf5cd10afaa9d0936d77c6ecd79a2
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-07-08 16:28:09 +0900

    add groups

--- sns/views.py
+++ sns/views.py
@@ -66,3 +66,76 @@ def index(request):
             'search_form':searchform,
         }
     return render(request, 'sns/index.html', params)
+
+@login_required(login_url='/admin/login/')
+def groups(request):
+    # 自分が登録したFriendを取得
+    friends = Friend.objects.filter(owner=request.user)
+    
+    # POST送信時の処理
+    if request.method == 'POST':
+        
+        # Groupsメニュー選択肢の処理
+        if request.POST['mode'] == '__groups_form__':
+            # 選択したGroup名を取得
+            sel_group = request.POST['groups']
+            # Grooupを取得
+            gp = Group.objects.filter(owner=request.user) \
+                .filter(title=sel_group).first()
+            # Groupに含まれるFriendを取得
+            fds = Friend.objects.filter(owner=request.user) \
+                .filter(group=gp)
+            # FriendのUserをリストにまとめる
+            vlist = []
+            for item in fds:
+                vlist.append(item.user.username)
+            # フォームの用意
+            groupsform = GroupSelectForm(request.user,request.POST)
+            friendsform = FriendsForm(request.user, \
+                    friends=friends, vals=vlist)
+        
+        # Friendsのチェック更新時の処理
+        if request.POST['mode'] == '__friends_form__':
+            # 選択したGroupの取得
+            sel_group = request.POST['group']
+            group_obj = Group.objects.filter(title=sel_group).first()
+            # チェックしたFriendsを取得
+            sel_fds = request.POST.getlist('friends')
+            # FriendsのUserを取得
+            sel_users = User.objects.filter(username__in=sel_fds)
+            # Userのリストに含まれるユーザーが登録したFriendを取得
+            fds = Friend.objects.filter(owner=request.user) \
+                    .filter(user__in=sel_users)
+            # すべてのFriendにGroupを設定し保存する
+            vlist = []
+            for item in fds:
+                item.group = group_obj
+                item.save()
+                vlist.append(item.user.username)
+            # メッセージを設定
+            messages.success(request, ' チェックされたFriendを' + \
+                    sel_group + 'に登録しました。')
+            # フォームの用意
+            groupsform = GroupSelectForm(request.user, \
+                    {'groups':sel_group})
+            friendsform = FriendsForm(request.user, \
+                    friends=friends, vals=vlist)
+    
+    # GETアクセス時の処理  
+    else:
+        # フォームの用意
+        groupsform = GroupSelectForm(request.user)
+        friendsform = FriendsForm(request.user, friends=friends, \
+                vals=[])
+        sel_group = '-'
+    
+    # 共通処理
+    createform = CreateGroupForm()
+    params = {
+            'login_user':request.user,
+            'groups_form':groupsform,
+            'friends_form':friendsform,
+            'create_form':createform,
+            'group':sel_group,
+        }
+    return render(request, 'sns/groups.html', params)
