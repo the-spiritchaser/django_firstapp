a89d7efb44510fb092c10b916a7c21ebb3836709
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-07-08 16:29:36 +0900

    add friend_add

--- sns/views.py
+++ sns/views.py
@@ -139,3 +139,36 @@ def groups(request):
             'group':sel_group,
         }
     return render(request, 'sns/groups.html', params)
+
+@login_required(login_url='/admin/login/')
+def add(request):
+    # 追加するUserを取得
+    add_name = request.GET['name']
+    add_user = User.objects.filter(username=add_name).first()
+    # Userが本人だった場合の処理
+    if add_user == request.user:
+        messages.info(request, "自分自身をFriendに追加することは\
+                できません。")
+        return redirect(to='/sns')
+    # publicの取得
+    (public_user, public_group) = get_public()
+    # add_userのFriendの数を調べる
+    frd_num = Friend.objects.filter(owner=request.user) \
+            .filter(user=add_user).count()
+    # ゼロより大きければ既に登録済み
+    if frd_num > 0:
+        messages.info(request, add_user.username + \
+                ' は既に追加されています。')
+        return redirect(to='/sns')
+    
+    # ここからFriendの登録処理
+    frd = Friend()
+    frd.owner = request.user
+    frd.user = add_user
+    frd.group = public_group
+    frd.save()
+    # メッセージを設定
+    messages.success(request, add_user.username + ' を追加しました！\
+        groupページに移動して、追加したFriendをメンバーに設定して下さい。')
+    return redirect(to='/sns')
+
