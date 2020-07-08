608af34aa2c678c4fa953d6faa285013875cadf7
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-07-08 16:31:38 +0900

    add share

--- sns/views.py
+++ sns/views.py
@@ -215,4 +215,40 @@ def post(request):
         }
     return render(request, 'sns/post.html', params)
 
+@login_required(login_url='/admin/login/')
+def share(request, share_id):
+    # シェアするMessageの取得
+    share = Message.objects.get(id=share_id)
     
+    # POST送信時の処理
+    if request.method == 'POST':
+        # 送信内容を取得
+        gr_name = request.POST['groups']
+        content = request.POST['content']
+        # Groupの取得
+        group = Group.objects.filter(owner=request.user) \
+                .filter(title=gr_name).first()
+        if group == None:
+            (pub_user, group) = get_public()
+        # メッセージを作成し、設定をして保存
+        msg = Message()
+        msg.owner = request.user
+        msg.group = group
+        msg.content = content
+        msg.share_id = share.id
+        msg.save()
+        share_msg = msg.get_share()
+        share_msg.share_count += 1
+        share_msg.save()
+        # メッセージを設定
+        messages.success(request, 'メッセージをシェアしました！')
+        return redirect(to='/sns')
+    
+    # 共通処理
+    form = PostForm(request.user)
+    params = {
+            'login_user':request.user,
+            'form':form,
+            'share':share,
+        }
+    return render(request, 'sns/share.html', params)
