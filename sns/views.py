9ec1f09bc4cece95ff7b1f81b7a6a00bb971ebe9
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-07-08 16:33:56 +0900

    add good

--- sns/views.py
+++ sns/views.py
@@ -252,3 +252,28 @@ def share(request, share_id):
             'share':share,
         }
     return render(request, 'sns/share.html', params)
+
+@login_required(login_url='/admin/login/')
+def good(request, good_id):
+    # goodするMessageを取得
+    good_msg = Message.objects.get(id=good_id)
+    # 自分がメッセージにGoodした数を調べる
+    is_good = Good.objects.filter(owner=request.user) \
+            .filter(message=good_msg).count()
+    # ゼロより大きければ既にgood済み
+    if is_good > 0:
+        messages.success(request, '既にメッセージにはGoodしています。')
+        return redirect(to='/sns')
+    
+    # Messageのgood_countを１増やす
+    good_msg.good_count += 1
+    good_msg.save()
+    # Goodを作成し、設定して保存
+    good = Good()
+    good.owner = request.user
+    good.message = good_msg
+    good.save()
+    # メッセージを設定
+    messages.success(request, 'メッセージにGoodしました！')
+    return redirect(to='/sns')
+
