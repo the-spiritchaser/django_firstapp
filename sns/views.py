3ae56f06b6d5fe496876e0027df97c5f4ea61a9f
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-07-08 16:31:01 +0900

    add post

--- sns/views.py
+++ sns/views.py
@@ -182,3 +182,37 @@ def creategroup(request):
     messages.info(request, '新しいグループを作成しました。')
     return redirect(to='/sns/groups')
 
+@login_required(login_url='/admin/login/')
+def post(request):
+    # POST送信の処理
+    if request.method == 'POST':
+        # 送信内容の取得
+        gr_name = request.POST['groups']
+        content = request.POST['content']
+        # Groupの取得
+        group = Group.objects.filter(owner=request.user) \
+                .filter(title=gr_name).first()
+        if group == None:
+            (pub_user, group) = get_public()
+        # Messageを作成し設定して保存
+        msg = Message()
+        msg.owner = request.user
+        msg.group = group
+        msg.content = content
+        msg.save()
+        # メッセージを設定
+        messages.success(request, '新しいメッセージを投稿しました！')
+        return redirect(to='/sns')
+    
+    # GETアクセス時の処理
+    else:
+        form = PostForm(request.user)
+    
+    # 共通処理
+    params = {
+            'login_user':request.user,
+            'form':form,
+        }
+    return render(request, 'sns/post.html', params)
+
+    
