b95448fc4bac5f88a9b3df087d5336fb82f0f329
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-07-15 23:05:24 +0900

    elif

--- hello/learn/practice.py
+++ hello/learn/practice.py
@@ -1,3 +1,4 @@
+# if文
 x = 123
 if x % 2 == 0:
     res = '偶数です。'
@@ -5,3 +6,20 @@ else:
     res = '奇数です。'
 str(x) + 'は、' + res
 
+# elif
+month = 8
+res = month // 3
+if month == 12:
+    res = 0
+
+if res== 0:
+    season = '冬'
+elif res == 1:
+    season ='春'
+elif res == 2:
+    season = '夏'
+elif res == 3:
+    season = '秋'
+else:
+    season  = '？'
+str(month) + '月は、' + season + 'です。'
